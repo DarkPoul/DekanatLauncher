2d20327cf80bb246fd447f583d23eeb663c96f42
S_varog <poulpotter1@gmail.com>
2022-09-02 10:19:19 +0300

    updateMainWindow

--- src/main/java/org/ioc/controller/ControllerMainWindow.java
+++ src/main/java/org/ioc/controller/ControllerMainWindow.java
@@ -1,21 +1,13 @@
 package org.ioc.controller;
 
 import java.io.IOException;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
-import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.input.KeyEvent;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.StackPane;
-import javafx.scene.layout.VBox;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
-import org.ioc.App;
 import org.ioc.Main;
 
 import static org.ioc.controller.ControllerApp.LoginName;
@@ -23,29 +15,15 @@ import static org.ioc.controller.ControllerApp.LoginName;
 
 public class ControllerMainWindow {
 
-
-    public static  VBox box;
-    public Label exit;
     public ImageView icon;
-
-
     @FXML
     public Label name;
 
     @FXML
     void initialize() {name.setText(LoginName);}
 
-    public void Registration_of_enrolled_students() throws IOException {
-        Stage stage = new Stage();
-        stage.setTitle("");
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("gui/Registration_of_enrolled_students.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 400, 200);
-        stage.initModality(Modality.APPLICATION_MODAL);
-        stage.setScene(scene);
-        stage.show();
-    }
-
     public void Support_of_the_educational_process() {
+
     }
 
     public void Making_changes_and_transferring_to_a_course() {
@@ -75,7 +53,7 @@ public class ControllerMainWindow {
         alert.showAndWait();
     }
 
-    public void Exit(ActionEvent event) {
+    public void Exit() {
         ((Stage) name.getScene().getWindow()).close();
     }
 }
